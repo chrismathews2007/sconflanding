0a74c19c7a6608d67d04b42ae865c88279388b9e
akhil varma gadiraju <akhilvarma1996@gmail.com>
2023-06-17 17:54:15 +0530

    ui changes

--- src/pages/health/index.jsx
+++ src/pages/health/index.jsx
@@ -1,54 +1,55 @@
-import React from 'react';
-import SEO from '@/components/seo';
-import Wrapper from '@/layout/wrapper';
-import Layout from '@/layout/womenLayout';
-import HomeBanner from '@/components/pages/women/home-banner';
-import ForumRegister from '@/components/pages/women/forumRegister';
+import React from "react";
+import SEO from "@/components/seo";
+import Wrapper from "@/layout/wrapper";
+import Layout from "@/layout/womenLayout";
+import HomeBanner from "@/components/pages/women/home-banner";
+import ForumRegister from "@/components/pages/women/forumRegister";
 
-import { client } from '@/components/contentful/client';
-import FaqArea from '@/components/pages/landing/faq';
-import Pricing from '@/components/pages/landing/pricing';
-import ForumTracks from '@/components/pages/landing/forumTracks';
-import Venue from '@/components/pages/women/venue';
-import { pricing_health } from '@/data/pricing-data';
+import { client } from "@/components/contentful/client";
+import FaqArea from "@/components/pages/landing/faq";
+import Pricing from "@/components/pages/landing/pricing";
+import ForumTracks from "@/components/pages/landing/forumTracks";
+import Venue from "@/components/pages/women/venue";
+import { pricing_health } from "@/data/pricing-data";
 
 const index = ({ faq }) => {
-	return (
-		<Wrapper>
-			<SEO pageTitle='Home' />
-			<Layout topHeader>
-				<div className=''>
-					<HomeBanner
-						title='World Health Forum'
-						gradient='bg-gradient-health-bottom'
-					/>
-					<ForumRegister gradient='text_h_gradient' />
-					<ForumTracks gradient='text_h_gradient' />
-					<Pricing
-						title='Register Now'
-						description='Most commonly asked questions by our attendees to help you make decision to attend most important event which you should not to miss to experience global dias'
-						themebg='gradient-health'
-						earlyBg
-						gradient='text_h_gradient'
-						pricing_data={pricing_health}
-					/>
-					<Venue gradient='text_h_gradient' />
-					<FaqArea faq={faq} gradient='text_h_gradient' />
-				</div>
-			</Layout>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <SEO pageTitle="Home" />
+      <Layout topHeader>
+        <div className="">
+          <HomeBanner
+            title="World Health Forum"
+            gradient="bg-gradient-health-bottom"
+            brochure="/assets/pdf/health.pdf"
+          />
+          <ForumRegister gradient="text_h_gradient" />
+          <ForumTracks gradient="text_h_gradient" />
+          <Venue gradient="text_h_gradient" />
+          <FaqArea faq={faq} gradient="text_h_gradient" />
+          <Pricing
+            title="Register Now"
+            description="Most commonly asked questions by our attendees to help you make decision to attend most important event which you should not to miss to experience global dias"
+            themebg="gradient-health"
+            earlyBg
+            gradient="text_h_gradient"
+            pricing_data={pricing_health}
+          />
+        </div>
+      </Layout>
+    </Wrapper>
+  );
 };
 
 export default index;
 
 export async function getStaticProps() {
-	const { items } = await client.getEntries({
-		content_type: 'faq',
-	});
-	return {
-		props: {
-			faq: items,
-		},
-	};
+  const { items } = await client.getEntries({
+    content_type: "faq",
+  });
+  return {
+    props: {
+      faq: items,
+    },
+  };
 }
